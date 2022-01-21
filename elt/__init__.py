d0f44c38423fb97f6e14250cfaeb85a5bd6ecc74
Pål Grønås Drange <Pal.Drange@uib.no>
2022-01-21 15:01:11 +0100

    Try basic textual example

--- elt/__init__.py
+++ elt/__init__.py
@@ -1,11 +1,31 @@
+from rich.panel import Panel
+
 from textual.app import App
+from textual.reactive import Reactive
+from textual.widget import Widget
 
 
-class Beeper(App):
-    def on_key(self):
-        self.console.bell()
+class Hover(Widget):
 
+    mouse_over = Reactive(False)
+
+    def render(self) -> Panel:
+        return Panel("Hello [b]World[/b]", style=("on red" if self.mouse_over else ""))
+
+    def on_enter(self) -> None:
+        self.mouse_over = True
+
+    def on_leave(self) -> None:
+        self.mouse_over = False
+
+
+class HoverApp(App):
+    """Demonstrates custom widgets"""
+
+    async def on_mount(self) -> None:
+        hovers = (Hover() for _ in range(10))
+        await self.view.dock(*hovers, edge="top")
 
 
 def main():
-    Beeper.run()
+    HoverApp.run(log="elt.log")
